806871e1188d73b556dc98dad9f6c37049eb5515
Chris Tralie <chris.tralie@gmail.com>
2021-06-13 10:27:26 -0400

    Adding art contest link and assignment description

---
+++ equations.js
@@ -0,0 +1,2 @@
+var eq1 = document.getElementById('eq1');
+eq1.innerHTML = "\\[ \\left[ \\begin{array}{c} x' \\\\ y' \\\\ z' \\end{array} \\right] = \\left[ \\begin{array}{ccc} a & 0 & 0 \\\\ 0 & b & 0 \\\\ 0 & 0 & c \\end{array} \\right] \\left[ \\begin{array}{c} x \\\\ y \\\\ z \\end{array} \\right] \\]";
